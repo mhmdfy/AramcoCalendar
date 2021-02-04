63e8a11b07218f285528f1a58ae7945b2abe46d8
Momo <mhmdfy@gmail.com>
2021-02-04 09:41:53 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/AramcoCalendar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.AramcoCalendar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Aramco Corporate Calendar',
+      home: Scaffold(
+        appBar: _getGradientAppBar(),
+        body: BodyPage(),
+      ),
+    );
+  }
+
+  AppBar _getGradientAppBar() {
+    return AppBar(
+      title: Text('Aramco Corporate Calendar'),
+      flexibleSpace: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [Color(0xff84bd00), Color(0xff00a3e0)],
+            // stops: [0.2, 2.5],
+          )
+        ),
+      ),
+    );
+  }
+}
+
+class BodyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(children: [Image(image: AssetImage(_getImageString(context)))]);
+  }
+
+  String _getImageString(BuildContext context) {
+    var shortestSide = MediaQuery.of(context).size.shortestSide;
+    var useMobileLayout = shortestSide < 600;
+
+    print('size is $shortestSide, use mobile is $useMobileLayout');
+
+    if (useMobileLayout) {
+      return 'assets/images/calendar-phone.png';
+    } else {
+      return 'assets/images/calendar-tablet.png';
+    }
+  }
+}
